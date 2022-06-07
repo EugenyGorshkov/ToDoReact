3cad4b1d223f2d60847edd8aff38c24adef5ae7d
EugenyGorshkov <gorshkov.zhenya@mail.ru>
2022-06-08 00:53:42 +0300

    1st commit npm-create and some features(store and design)

---
+++ src/data/stores/useToDoStore.ts
@@ -0,0 +1,57 @@
+import create from "zustand";
+import { generateId } from './../helper';
+
+
+interface Task {
+    id: string;
+    title: string;
+    createdAt: number;
+}
+
+interface ToDoStore {
+    tasks: Task[];
+    createTask: (title: string) => void;
+    updateTask: (id: string, title: string) => void;
+    removeTask: (id: string) => void;
+}
+
+export const useToDoStore = create<ToDoStore>((set, get) => ({
+    tasks: [
+        {
+            id: '123dqwd12d21d',
+            title: 'Default task',
+            createdAt: 123155112412,
+        },
+        {
+            id: '2131333qdqwd12d21d',
+            title: 'Default task 2',
+            createdAt: 123155112412,
+        }
+    ],
+    createTask:(title: string) => {
+        const { tasks } = get();
+        const newTask = {
+            id: generateId(),
+            title,
+            createdAt: Date.now(),
+        }
+        set({
+            tasks: [newTask].concat(tasks),
+        })
+    },
+    updateTask:(id: string, title: string) => {
+        const { tasks } = get();
+        set({
+            tasks: tasks.map(task => ({
+                ...task,
+                title: task.id === id ? title : task.title,
+            }))
+        });
+    },
+    removeTask:(id: string) => {
+        const { tasks } = get();
+        set({
+            tasks: tasks.filter((task) => task.id !== id)
+        });
+    },
+})) 
